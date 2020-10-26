226349063ec21d4304d452d5cc9bc50f0db0b333
qsparks <q.m.sparks@gmail.com>
2020-10-26 17:34:27 -0500

    adds login functionality using jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,8 +26,49 @@ const register = async (req, res) => {
     }
 } 
 
+// ANCHOR Login Route
+const login = async (req, res) => {
+    try {
+        const foundUser = await db.User.findOne({ email: req.body.email });
+
+        if (!foundUser) {
+            return res.send({ message: "Email or Password incorrect" });
+        }
+
+        const match = await bcrypt.compare(req.body.password, foundUser.password);
+
+        if(!match) {
+            return res.send({message: "Email or password incorrect"});
+        } else {
+            const signedJwt = await jwt.sign(
+                {
+                    _id: foundUser._id,
+                },
+                "dev_key",
+                {
+                    expiresIn: "24h",
+                }
+            );
+            return res.status(200).json({
+                status: 200,
+                message: "Success",
+                id: foundUser._id,
+                signedJwt
+            });
+        }
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            status: 500,
+            message: "Something went wrong. Please try again."
+        })
+    }
+}
+
 
 module.exports = {
     register,
+    login,
     
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const ctrl = require('../controllers');
 
 router.post('/register', ctrl.auth.register);
-// router.post('/login', ctrl.auth.login);
+router.post('/login', ctrl.auth.login);
 // router.post('/logout', ctrl.auth.logout);
 
 module.exports = router;
