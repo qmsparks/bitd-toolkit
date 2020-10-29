d1f299e2070f4691d1a5980ac1845b3c4bf5640b
qsparks <q.m.sparks@gmail.com>
2020-10-29 10:31:23 -0500

    tool show route working

--- controllers/tools.js
+++ controllers/tools.js
@@ -4,22 +4,24 @@ const utils = require('../utils');
 
 
 const index = async (req, res) => {
-    db.User.findById(req.userId)
-        .populate({
-            path: 'tools',
-            populate: {path: 'components'}
-        })
-        .exec(function (err, foundUser){
-            if (err) {
-                console.log(err);
-                return res.status(500).json({
-                    status: 500,
-                    message: "Something went wrong. please try again."
-                })
-            }
-            console.log(foundUser);
+    try {
+        const foundUser = await db.User.findById(req.userId)
+            .populate({
+                path: 'tools',
+                populate: {path: 'components'}
+            });
             res.status(200).json({"tools": foundUser.tools});
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            status: 500,
+            message: "Something went wrong. please try again."
         })
+    }
+
+
 }
 
 
@@ -33,8 +35,23 @@ const details = (req, res) => {
     res.status(200).json({"details": details});
 }
 
-const show = (req, res) => {
+const show = async (req, res) => {
+    try {
+        const foundTool = await db.Tool.findById(req.params.id)
+            .populate('components');
 
+        if (!foundTool) return res.status(200).json({"message": "No tool with that id found in db"});
+
+        res.status(200).json({"tool": foundTool});
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            status: 500,
+            message: "Something went wrong. please try again."
+        })
+    }
 }
 
 // NOTE on the front end, will need to make sure that this request is sending component ids
--- routes/tools.js
+++ routes/tools.js
@@ -5,13 +5,15 @@ const authRequired = require('../middleware/authRequired');
 
 // ANCHOR routes
 // routes currently utilized
+router.get('/', authRequired, ctrl.tools.index);
 router.get('/generate/:tool', ctrl.tools.generate);
 router.get('/details/:tool', ctrl.tools.details);
 router.post('/', authRequired, ctrl.tools.create);
+router.get('/:id', ctrl.tools.show);
+
+// routes in progress
 
 // routes to come
-router.get('/', authRequired, ctrl.tools.index);
-router.get('/:id', ctrl.tools.show);
 router.put('/:id', authRequired, ctrl.tools.update);
 router.delete('/:id', authRequired, ctrl.tools.destroy);
 
